277b82ecb8c60aa48c3b37cc0121348ae39ffe69
ibrahim ahmed <ib.farag@gmail.com>
2026-03-17 15:48:28 +0200

    Add screenshot for hatch-concept-studio theme

---
+++ platform/themes/hatch-concept-studio/src/Http/Controllers/HatchConceptStudioController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Theme\HatchConceptStudio\Http\Controllers;
+
+use Botble\Theme\Http\Controllers\PublicController;
+
+class HatchConceptStudioController extends PublicController
+{
+}
