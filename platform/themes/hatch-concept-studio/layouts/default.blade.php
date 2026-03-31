c925e93130d56e891a1fd2eed1c7faaeed4eac53
ibrahim ahmed <ib.farag@gmail.com>
2026-03-31 22:50:59 +0200

    feat: Refactor layout structure and improve indentation in default template

--- platform/themes/hatch-concept-studio/layouts/default.blade.php
+++ platform/themes/hatch-concept-studio/layouts/default.blade.php
@@ -1,5 +1,9 @@
 {!! Theme::partial('header') !!}
+<div id="main" style="padding-top: 132px;">
+    <div class="container w-md-75">
 
-    {!! Theme::content() !!}
+        {!! Theme::content() !!}
+    </div>
+</div>
 
 {!! Theme::partial('footer') !!}
