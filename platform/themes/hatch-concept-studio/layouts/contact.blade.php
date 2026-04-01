3088b2c6200ebe422b0f6ddb49c64ad5fe37afaf
ibrahim ahmed <ib.farag@gmail.com>
2026-04-01 18:40:44 +0200

    refactor: Remove unused functions and layout files for cleaner codebase

--- platform/themes/hatch-concept-studio/layouts/contact.blade.php
+++
@@ -1,7 +0,0 @@
-{!! Theme::partial('header') !!}
-
-<div id="main">
-    {!! Theme::content() !!}
-</div>
-
-{!! Theme::partial('footer') !!}
--- platform/themes/hatch-concept-studio/layouts/normal-page.blade.php
+++
@@ -1,9 +0,0 @@
-{!! Theme::partial('header') !!}
-
-<div id="main">
-    {{-- <div class="page-content"> --}}
-        {!! Theme::content() !!}
-    {{-- </div> --}}
-</div>
-
-{!! Theme::partial('footer') !!}
